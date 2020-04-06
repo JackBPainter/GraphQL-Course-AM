1b4c74c7a06b0c1fa3cb45262e1458be71fa81b7
Jack Painter <littlesympathy@jacks-mbp.home>
2020-04-06 14:44:24 +0100

    Complete import & export challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Welcome to the graphQL course')
+import myDefault, { subtract } from './math'
+
+const num1 = 20
+const num2 = 10
+
+console.log(myDefault(num1, num2))
+console.log(subtract(num1, num2))

f70a8b5e61c382505d735e9df12192876182c0e7
Jack Painter <littlesympathy@jacks-mbp.home>
2020-04-07 17:32:23 +0100

    Intro to relational data and set up comments query

--- src/index.js
+++ src/index.js
@@ -2,13 +2,66 @@ import { GraphQLServer } from 'graphql-yoga'
 
 // Scaler types - String, Boolean, Int (whole numbers), Float (numbers with decimal points, ID) 
 
+// Demo user data
+const comments = [{
+    id: '101',
+    text: 'This is comment one'
+}, {
+    id: '102',
+    text: 'This is comment two'
+}, {
+    id: '103',
+    text: 'This is comment three'
+}, {
+    id: '104',
+    text: 'This is comment four'
+}]
+
+const users = [{
+    id: '1',
+    name: 'Jess',
+    email: 'Jess@gmail.com',
+    age: 32
+}, {
+    id: '2',
+    name: 'Mark',
+    email: 'Mark@yahoo.com',
+    age: 24
+}, {
+    id: '3', 
+    name: 'Barnett',
+    email: 'Barnett@aol.com',
+    age: 27
+}]
+
+const posts = [{
+    id: '1',
+    title: 'First World War',
+    body: 'The first of two world wars',
+    published: true,
+    author: '1'
+}, {
+    id: '2',
+    title: 'Second World War',
+    body: 'The second of two world wars',
+    published: true,
+    author: '1'
+}, {
+    id: '3',
+    title: 'Coronavirus',
+    body: 'The war against people who are too stupid / ignorant to listen to government instructions',
+    published: true,
+    author: '2'
+}]
+
 // Type Defintion = Schema
 const typeDefs = `
     type Query {
-        greeting(name: String): String!
-        add(num1: Float!, num2: Float!): Float!
-        post: Post!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
         me: User!
+        post: Post!
+        comments: [Comment!]!
     }
 
     type User {
@@ -16,6 +69,7 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
     }
 
     type Post {
@@ -23,21 +77,36 @@ const typeDefs = `
         title: String!
         body: String!
         published: Boolean!
+        author: User!
+    }
+
+    type Comment {
+        id: ID!
+        text: String!
     }
 `
 
 // Resolvers
 const resolvers = {
     Query: {
-        greeting(parents, args) {
-            if (args.name) {
-                return `Hello ${args.name}!`
-            } else {
-                return 'Hello!'
+        users(parent, args, ctx, info) {
+            if (!args.query) {
+                return users
             }
-        },  
-        add(parents, args) {
-            return args.num1 + args.num2
+
+            return users.filter((user) => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
+            })
+        },
+        posts(parent, args, ctx, info) {
+            if (!args.query) {
+                return posts
+            }
+
+            return posts.filter((post) => {
+                return post.body.toLowerCase().includes(args.query.toLowerCase()) || 
+                    post.title.toLowerCase().includes(args.query.toLowerCase())
+            })
         },
         me() {
             return {
@@ -54,6 +123,23 @@ const resolvers = {
                 body: 'This is my first blog post',
                 published: true
             }
+        },
+        comments(parent, args, ctx, info) {
+            return comments
+        }
+    },
+    Post: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        }
+    },
+    User: {
+        posts(parent, args, ctx, info) {
+            return posts.filter((post) => {
+                return post.author === parent.id
+            })
         }
     }
 }

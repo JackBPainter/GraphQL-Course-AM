d9b963292842ee3a7ad1f2dfdb118651220988fa
Jack Painter <littlesympathy@jacks-mbp.home>
2020-04-10 12:27:58 +0100

    Create basic count subscription

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import { GraphQLServer } from 'graphql-yoga'
+import { GraphQLServer, PubSub } from 'graphql-yoga'
 import db from './db'
 import Query from './resolvers/Query'
 import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
 import User from './resolvers/User'
 import Post from './resolvers/Post'
 import Comment from './resolvers/Comment'
 
-// Scaler types - String, Boolean, Int (whole numbers), Float (numbers with decimal points, ID) 
+const pubsub = new PubSub()
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers: {
         Query,
         Mutation,
+        Subscription,
         User,
         Comment,
         Post,
     },
     context: {
-        db
+        db,
+        pubsub
     }
 })
 
